b7925a4349851aec5bab98ffab13316c98ed4d63
satyam-kr03 <b22236@students.iitmandi.ac.in>
2024-04-24 09:09:44 +0530

    added camera and chat pages

---
+++ lib/Pages/api_key.dart
@@ -0,0 +1,3 @@
+class GeminiApiKey {
+  static const api_key = "AIzaSyDMC2KHaVz4bh1pZ6VjOLoSptwzJ0R2hLk";
+}
---
+++ lib/Pages/main.dart
@@ -0,0 +1,30 @@
+import 'package:camera/camera.dart';
+import 'package:image_speak/camera_page.dart';
+import 'package:flutter/material.dart';
+import 'chat.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+  runApp(MainApp(cameras: cameras));
+}
+
+class MainApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
+  const MainApp({super.key, required this.cameras});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(
+        cameras: cameras,
+      ),
+      // home: ChatScreen(),
+    );
+  }
+}
+
+
+
